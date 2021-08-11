b3851c7c0646e377b228b55e612125efc911b80a
ttnhat17it3 <38066091+ttnhat17it3@users.noreply.github.com>
2021-08-11 19:09:01 +0700

    update bai 1

--- Bai1/index.js
+++ Bai1/index.js
@@ -1,15 +1,3 @@
-// Select current date at birthday
-const date = new Date();
-const currentDay = date.getDay();
-const currentMonth = date.getMonth();
-const currentYear = date.getFullYear();
-
-// document.querySelector("#birthday_day").value = currentDay;
-// document.querySelector("#birthday_month").value = currentMonth;
-// document.querySelector("#birthday_year").value = currentYear;
-
-console.log(`${currentDay} ${currentMonth} ${currentYear}`);
-
 const modal = document.querySelector(".js-modal");
 modal.addEventListener("click", closeModal);
 
@@ -25,12 +13,16 @@ openModalBtn.addEventListener("click", openModal);
 const closeModalBtn = document.querySelector(".js-close-modal");
 closeModalBtn.addEventListener("click", closeModal);
 
+const genderOption = document.querySelector(".js-gender-option");
+
 const lastNameWarning = document.querySelector(".js-last-name-warning");
 const firstNameWarning = document.querySelector(".js-first-name-warning");
 const phoneEmailWarning = document.querySelector(".js-phone-email-warning");
 const passwordWarning = document.querySelector(".js-password-warning");
 const birthdayWarning = document.querySelector(".js-birthday-warning");
 const genderWarning = document.querySelector(".js-gender-warning");
+
+const genders = document.querySelectorAll("#gender");
 
 // Get value of input
 
@@ -42,59 +34,150 @@ function openModal() {
 }
 
 function closeModal() {
-  console.log("close");
   modal.classList.remove("open");
 }
 
 function getInput() {
+  // Validate name
   const lastName = document.querySelector("#last-name");
-  if (!lastName.value) {
-    lastName.style.border = "1px solid red";
-    document.querySelector(".js-last-name-warning").classList.remove("hidden");
-  }
-
   const firstName = document.querySelector("#first-name");
+  // validateName(firstName, lastName);
+  if (!lastName.value) {
+    document.querySelector("#last-name").style.border = "1px solid red";
+    document.querySelector(".js-last-name-warning").classList.remove("hidden");
+  } else {
+    document.querySelector("#last-name").style.border = "1px solid #ccc;";
+    document.querySelector(".js-last-name-warning").classList.add("hidden");
+  }
+
   if (!firstName.value) {
-    firstName.style.border = "1px solid red";
+    document.querySelector("#first-name").style.border = "1px solid red";
     document.querySelector(".js-first-name-warning").classList.remove("hidden");
+  } else {
+    document.querySelector("#first-name").style.border = "1px solid #ccc;";
+    document.querySelector(".js-first-name-warning").classList.add("hidden");
   }
 
+  // Validate phone and e-mail
   const phoneEmail = document.querySelector("#phone-email");
-  if (!phoneEmail.value) {
-    phoneEmail.style.border = "1px solid red";
-    document
-      .querySelector(".js-phone-email-warning")
-      .classList.remove("hidden");
-  }
+  validatePhoneEmail(phoneEmail);
 
+  // Validate password
   const password = document.querySelector("#password");
-  if (!password.value) {
-    password.style.border = "1px solid red";
-    document.querySelector(".js-password-warning").classList.remove("hidden");
-  }
+  validatePassword(password);
 
+  // Validate birthday
   const day = document.querySelector("#day");
   const month = document.querySelector("#month");
   const year = document.querySelector("#year");
+  validateBirthday(day.value, month.value, year.value);
 
-  if (!validateBirthday(day.value, month.value, year.value)) {
-    console.log("ko duoc");
-    document.querySelector(".js-birthday-warning").classList.remove("hidden");
+  // Validate gender
+  const gender = $("input[type='radio'][name='gender']:checked").val();
+  validateGender(gender);
+  let callName = document.querySelector("#gender-option");
+  console.log(`Danh xung: ${callName.value}`);
+  console.log(`Gender: ${gender}`);
+  console.log(callName.value == 0);
+  if (callName.value == 0 || !callName.value) {
+    // Options
+    console.log("Chon di");
+    document.querySelector("#gender-option").style.border = "1px solid red";
+  } else {
+    document.querySelector("#gender-option").style.border = "1px solid #ccc";
   }
+
+  // if (!validateBirthday(day.value, month.value, year.value)) {
+  //   console.log("ko duoc");
+  //   document.querySelector(".js-birthday-warning").classList.remove("hidden");
+  // }
   // const gender = document.querySelector('input[name="gender"]:checked').value;
 }
 
-function validateBirthday(day, month, year) {
-  console.log(`${day} ${month} ${year}`);
-  if (day === 0 || month === 0 || year === 0) {
-    console.log("thieu");
-    return false;
+function validateName(firstName, lastName) {
+  if (!lastName.value) {
+    lastName.style.border = "1px solid red";
+    document.querySelector(".js-last-name-warning").classList.remove("hidden");
+  } else {
+    lastName.style.border = "1px solid #ccc;";
+    document.querySelector(".js-last-name-warning").classList.add("hidden");
   }
-  return true;
+
+  if (!firstName.value) {
+    firstName.style.border = "1px solid red";
+    document.querySelector(".js-first-name-warning").classList.remove("hidden");
+  } else {
+    firstName.style.border = "1px solid #ccc;";
+    document.querySelector(".js-first-name-warning").classList.add("hidden");
+  }
 }
 
-function notify() {
-  console.log("ahhi");
+function validatePhoneEmail(phoneEmail) {
+  const phoneRegex = /((09|03|07|08|05)+([0-9]{8})\b)/g;
+  const emailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  if (phoneRegex.test(phoneEmail.value) || emailRegex.test(phoneEmail.value)) {
+    phoneEmailWarning.classList.add("hidden");
+    document.querySelector("#phone-email").style.border = "1px solid #ccc";
+  } else {
+    phoneEmailWarning.classList.remove("hidden");
+    document.querySelector("#phone-email").style.border = "1px solid red";
+  }
+}
+
+function validatePassword(password) {
+  if (!password.value || password.value.length < 6) {
+    document.querySelector("#password").style.border = "1px solid red";
+    passwordWarning.classList.remove("hidden");
+  } else {
+    document.querySelector("#password").style.border = "1px solid #ccc";
+    passwordWarning.classList.add("hidden");
+  }
+}
+
+function isValidDate(date) {
+  return date > 0;
+}
+
+function validateBirthday(day, month, year) {
+  if (!isValidDate(day) || !isValidDate(month) || !isValidDate(year)) {
+    document.querySelector("#year").style.border = "1px solid red";
+    document.querySelector("#day").style.border = "1px solid red";
+    document.querySelector("#month").style.border = "1px solid red";
+    birthdayWarning.classList.remove("hidden");
+  } else {
+    document.querySelector("#year").style.border = "1px solid #ccc";
+    document.querySelector("#day").style.border = "1px solid #ccc";
+    document.querySelector("#month").style.border = "1px solid #ccc";
+    birthdayWarning.classList.add("hidden");
+  }
+}
+
+function notify(e) {
+  if (e === 2) {
+    genderOption.classList.remove("hidden");
+  } else {
+    genderOption.classList.add("hidden");
+  }
+}
+
+function validateGender(gender) {
+  if (!gender) {
+    genderWarning.classList.remove("hidden");
+    document.querySelector(".input-female").style.border = "1px solid red";
+    document.querySelector(".input-male").style.border = "1px solid red";
+    document.querySelector(".input-option").style.border = "1px solid red";
+  } else {
+    genderWarning.classList.add("hidden");
+    document.querySelector(".input-female").style.border = "1px solid #ccc";
+    document.querySelector(".input-male").style.border = "1px solid #ccc";
+    document.querySelector(".input-option").style.border = "1px solid #ccc";
+  }
+}
+
+function validateGenderOption() {
+  const appellation = document.querySelector("#gender-option");
+  console.log(`Danh xung: ${appellation}`);
 }
 
 function showWarning(input) {
