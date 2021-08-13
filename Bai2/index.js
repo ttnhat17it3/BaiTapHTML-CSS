2e4224daa60e769325625fa6af6ff74b2d6bf1d5
ttnhat17it3 <38066091+ttnhat17it3@users.noreply.github.com>
2021-08-13 16:10:16 +0700

    update bai 2

--- Bai2/index.js
+++ Bai2/index.js
@@ -3,6 +3,11 @@ const thirdStory = document.getElementsByClassName("story")[2];
 const secondStory = document.getElementsByClassName("story")[1];
 const firstStory = document.getElementsByClassName("story")[0];
 
+const firstName = firstStory.childNodes[1];
+const secondName = secondStory.childNodes[1];
+const thirdName = thirdStory.childNodes[1];
+const lastName = lastStory.childNodes[1];
+
 const nextButton = document.getElementsByClassName("story")[3].childNodes[5];
 
 const storyContainer = document.getElementsByClassName("story");
@@ -16,9 +21,10 @@ function responsive645(x) {
   if (x.matches && thirdStory.hasChildNodes(nextButton)) {
     // If media query matches
     thirdStory.appendChild(nextButton);
-    // lastStory.style.display = none;
+    lastName.style.display = "none";
   } else {
     thirdStory.removeChild(nextButton);
+    lastName.style.display = "inline-block";
   }
 }
 
@@ -26,8 +32,10 @@ function responsive525(x) {
   if (x.matches && secondStory.hasChildNodes(nextButton)) {
     // If media query matches
     secondStory.appendChild(nextButton);
+    thirdName.style.display = "none";
   } else {
     secondStory.removeChild(nextButton);
+    thirdName.style.display = "inline-block";
   }
 }
 
@@ -35,8 +43,10 @@ function responsive405(x) {
   if (x.matches && firstStory.hasChildNodes(nextButton)) {
     // If media query matches
     firstStory.appendChild(nextButton);
+    secondName.style.display = "none";
   } else {
     firstStory.removeChild(nextButton);
+    secondName.style.display = "inline-block";
   }
 }
 
